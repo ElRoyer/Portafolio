bd4f8faf6d5b8da1b0288d2a5750f96328a7e383
ElRoyer <royy.hdez@gmail.com>
2022-05-09 09:37:35 -0500

    actualizacion

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
             <article className='about__card'>
               <FiUsers className='about__icon'/>
               <h5>Clientes</h5>
-              <small>10+ En todo el mundo</small>
+              <small>5+ Diferentes Empresas</small>
             </article>
 
             <article className='about__card'>
